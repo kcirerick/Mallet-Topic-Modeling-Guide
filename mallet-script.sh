9038a1f2c6194f3811505a783f2ab9ad5ef951ce
Erick Enriquez <enriquezerick182@gmail.com>
2019-08-15 14:16:44 -0700

    Delete mallet-script.sh

--- mallet-script.sh
+++
@@ -1,9 +0,0 @@
-python ./python-stuff/load-conversation-files.py
-bin/mallet import-dir --input python-stuff/conversationResults --output mytrans.mallet --keep-sequence --stoplist-file stoplists/all-stops.txt
-for i in 1 2 3 4 5
-do
-KEYS="topic_keys_$i.txt"
-STATE="topic_state_$i.gz"
-COMP="transcript_composition_$i.txt"
-bin/mallet train-topics --input mytrans.mallet --num-topics 300 --output-state $STATE --output-topic-keys $KEYS --output-doc-topics $COMP
-done
